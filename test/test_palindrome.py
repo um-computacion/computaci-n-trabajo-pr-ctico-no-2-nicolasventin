7eb49c2af726c6eef2600d23ff9a9a6cb7c9a26d
Nico <nve2004@gmail.com>
2025-04-16 11:44:11 -0300

    Agregar tests para frases palíndromas

--- test/test_palindrome.py
+++ test/test_palindrome.py
@@ -11,5 +11,16 @@ class TestPalindromosSimples(unittest.TestCase):
     def test_radar(self):
         self.assertTrue(is_palindrome("radar"))
 
+class TestFrasesPalindromas(unittest.TestCase):
+
+    def test_anita(self):
+        self.assertTrue(is_palindrome("Anita lava la tina"))
+
+    def test_mama_roma(self):
+        self.assertTrue(is_palindrome("A mamá Roma le aviva el amor a mamá"))
+
+    def test_ruta(self):
+        self.assertTrue(is_palindrome("La ruta nos aportó otro paso natural"))
+        
 if __name__ == '__main__':
     unittest.main()
